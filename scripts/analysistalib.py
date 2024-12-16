645ac1f718cb2b14b3ae8f0d2b5ef8eb24ddb8a2
Bezawit Girma <gahebs@gmail.com>
2024-12-16 12:04:20 +0300

    add talib analysis files

--- scripts/analysistalib.py
+++ scripts/analysistalib.py
@@ -2,9 +2,14 @@ import pandas as pd
 import talib
 import matplotlib.pyplot as plt
 
-# Load your stock price data into a DataFrame
+# Load Meta historical data into a DataFrame
 df = pd.read_csv('D:/week1 data/yfinance_data/META_historical_data.csv')
 
+# Check if the required columns are present 
+required_columns = ['Open', 'High', 'Low', 'Close', 'Volume'] 
+missing_columns = [col for col in required_columns if col not in df.columns] 
+if not missing_columns: print("All required columns are present.") 
+else: print(f"Missing columns: {missing_columns}") 
 
 # Calculate Simple Moving Average (SMA)
 df['SMA_20'] = talib.SMA(df['Close'], timeperiod=20)
@@ -13,13 +18,10 @@ df['SMA_20'] = talib.SMA(df['Close'], timeperiod=20)
 df['EMA_20'] = talib.EMA(df['Close'], timeperiod=20)
 
 # Calculate Relative Strength Index (RSI)
-df['RSI'] = talib.RSI(df['Close'], timeperiod=14)
-
-# Calculate Bollinger Bands
-# df['Upper Band'], df['Middle Band'], df['Lower Band'] = talib.BBANDS(df['Close'], timeperiod=20)
+df['RSI'] = talib.RSI(df['Close'], timeperiod=20)
 
 # Calculate Moving Average Convergence Divergence (MACD)
-# df['MACD'], df['MACD Signal'], df['MACD Hist'] = talib.MACD(df['Close'])
+df['MACD'], df['MACD Signal'], df['MACD Hist'] = talib.MACD(df['Close'])
 
 # Display the DataFrame with the new indicators
 print(df.tail())
@@ -27,10 +29,18 @@ print(df.tail())
 # Save the DataFrame to a new CSV file with indicators
 # df.to_csv('path/to/your/stock_data_with_indicators.csv', index=False)
 
-# Optional: Plot the closing prices and a technical indicator
+# Plot the closing prices and a technical indicator of SMA
 plt.figure(figsize=(14, 7))
 plt.plot(df['Close'], label='Close Price')
 plt.plot(df['SMA_20'], label='20-day SMA')
 plt.title('Stock Price and SMA')
+plt.legend()
+plt.show()
+
+# Plot the closing prices and a technical indicator of EMA
+plt.figure(figsize=(14, 7))
+plt.plot(df['Close'], label='Close Price')
+plt.plot(df['RSI'], label='RSI')
+plt.title('Stock Price and RSI')
 plt.legend()
 plt.show()
